23b1c0d311e0d965b1d811ccaadb76ed43dfc9b9
Ceaica Tatiana <ceaica.tatiana@gmail.com>
2020-10-27 16:52:19 +0200

    Set the clock to rotate

---
+++ script.js
@@ -0,0 +1,13 @@
+setInterval(setClock,1000);
+
+const hourHand = document.querySelector('[data-hour-hand]')
+const minuteHand = document.querySelector('[data-minute-hand]')
+const secondHand = document.querySelector('[data-second-hand]')
+
+function setClock(){
+  const currentDate = newDate()
+  const secondsRatio = currentDate.getseconds() / 60
+  const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
+  const hoursRatio = (minutesRatio + currentDate.getHours()) / 12
+
+}
